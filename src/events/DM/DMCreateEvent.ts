9af100b9e763a3c174e572b7c2a9e739f735bd2f
Ar Rakin <rakinar2@gmail.com>
2022-09-24 15:19:43 +0600

    chore: partial commit

--- src/events/DM/DMCreateEvent.ts
+++ src/events/DM/DMCreateEvent.ts
@@ -4,6 +4,7 @@ import DiscordClient from '../../client/Client';
 import MessageEmbed from '../../utils/MessageEmbed';
 import Thread from '../../models/Thread';
 import { getChannel } from '../../utils/utils';
+import { MessageOptions } from 'child_process';
 
 export default class DMCreateEvent extends BaseEvent {
     constructor() {
@@ -14,9 +15,16 @@ export default class DMCreateEvent extends BaseEvent {
         const { author } = message;
         const thread = await Thread.findOne({ user: author.id });
 
+        const messageOptions: MessageOptions = {
+            
+        };
+
         if (thread) {
             const channel = await getChannel(client, thread.channel);
             
+            if (channel) {
+                
+            }
         }
     }
 }
